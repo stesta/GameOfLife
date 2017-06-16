ee64445cc0c73b76de05fbe2a3aaa67045b2bd1e
Steve Testa <steve@testasoftware.com>
2017-06-16 12:34:51 -0400

    some initial React/HTML5 experiments

--- assets/scripts/app.jsx
+++ assets/scripts/app.jsx
@@ -3,9 +3,11 @@ class GameBoard extends React.Component {
         super(props);
         this.state = {
             counter: 0,
-            board: '',
+            board: [],
             nextGenerationDelay: 200,
-            running: false
+            running: false,
+            boardWidth: 400,
+            boardHeight: 400
         };
     }
 
@@ -20,9 +22,9 @@ class GameBoard extends React.Component {
 
     nextGeneration(ws) {
         setTimeout(() => {
-            ws.send(self.state.counter);
-            self.setState({ counter: self.state.counter+1 });
-        }, self.state.nextGenerationDelay);
+            ws.send(this.state.counter);
+            this.setState({ counter: this.state.counter+1 });
+        }, this.state.nextGenerationDelay);
     }
 
     drawBoard() {
@@ -30,11 +32,12 @@ class GameBoard extends React.Component {
     }
 
     startGame() {
+        var self = this;
+        
         if (self.state.running === false)
         {
-            var self = this;
             self.setState({ running: true });
-            var ws = new WebSocket(getWebsocketUri());
+            var ws = new WebSocket(self.getWebsocketUri());
 
             ws.onerror = (event) => {
                 self.setState({ 
@@ -45,7 +48,7 @@ class GameBoard extends React.Component {
 
             ws.onopen = () => {
                 self.setState({ board: 'WebSockets connection successful!' });
-                nextGeneration(ws);
+                self.nextGeneration(ws);
             };
 
             ws.onclose = () => {
@@ -56,14 +59,51 @@ class GameBoard extends React.Component {
             };
 
             ws.onmessage = (event) => {
-                self.setState({ board: event.data });
-                nextGeneration(ws);
+                var b = JSON.parse(event.data);
+                self.setState({ board: b });
+                this.drawGrid();
+
+                self.nextGeneration(ws);
+                
             };
         }
     }
 
+    searchForArray(haystack, needle){
+        var i, j, current;
+        for(i = 0; i < haystack.length; ++i){
+            if(needle.length === haystack[i].length){
+            current = haystack[i];
+            for(j = 0; j < needle.length && needle[j] === current[j]; ++j);
+            if(j === needle.length)
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    drawGrid() { 
+        var rectSize = 10;
+        var c = document.getElementById('board');
+        var ctx = c.getContext('2d');
+            ctx.clearRect(0, 0, 400, 400);
+
+        for (var j = 1; j < 400; j++) { 
+        for (var k = 1; k < 400; k++) {
+            var elem = [j-5,k-5]
+            ctx.fillStyle = '#333333';
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = '#999999';
+            ctx.strokeRect((j*rectSize)-1, (k*rectSize)-1, rectSize, rectSize); 
+            
+            if (this.searchForArray(this.state.board, elem) != -1) {
+                ctx.fillRect((j*rectSize)-1, (k*rectSize)-1, rectSize, rectSize);
+            }  
+        }}
+    }
+
     componentDidMount() {
-        startGame();
+        this.startGame();
     }
 
     render() {
